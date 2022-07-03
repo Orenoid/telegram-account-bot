4030d70002ae501c3c207b0506395cd1dcbcf659
Orenoid <stefu1225@gmail.com>
2022-07-03 23:30:28 +0800

    feat: 账单渲染组件

--- telebot/senders.go
+++ telebot/senders.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/orenoid/telegram-account-bot/models"
 	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
 	"gopkg.in/telebot.v3"
+	"sort"
+	"strings"
 )
 
 type NewBillSender struct {
@@ -26,6 +29,10 @@ func (sender *NewBillSender) Send(bot *telebot.Bot, recipient telebot.Recipient,
 	return bot.Send(recipient, template.Render(), opts)
 }
 
+type Template interface {
+	Render() string
+}
+
 type BillCreatedTemplate struct {
 	bill *models.Bill
 }
@@ -35,4 +42,166 @@ func (template *BillCreatedTemplate) Render() string {
 		`账单金额：%s 元，类别：%s
 点击 "撤销" 可撤回该账单并回滚余额`
 	return fmt.Sprintf(format, template.bill.Amount.String(), template.bill.Category)
+}
+
+type BillListTemplate struct {
+	Bills         []*models.Bill
+	MergeCategory bool // 对相同类别的账单求总和后展示
+}
+
+func (template *BillListTemplate) Render() string {
+	if len(template.Bills) == 0 {
+		return "暂无收支记录"
+	}
+
+	result := &strings.Builder{}
+	expendSection := template.expendSection()
+	if len(expendSection) > 0 {
+		result.Write([]byte(expendSection))
+	}
+	incomeSection := template.incomeSection()
+	if len(expendSection) > 0 && len(incomeSection) > 0 {
+		// 支出与收入记录之间多间隔一行
+		result.Write([]byte("\n\n"))
+	}
+	if len(incomeSection) > 0 {
+		result.Write([]byte(incomeSection))
+	}
+
+	return result.String()
+}
+
+// expendSection 支出记录
+func (template *BillListTemplate) expendSection() string {
+	expendSum := decimal.NewFromFloat(0)
+	categoryMapping := map[string]decimal.Decimal{}
+	var expendBills []*models.Bill
+
+	// 筛选支出账单
+	for _, bill := range template.Bills {
+		if bill.Amount.LessThan(decimal.NewFromFloat(0)) {
+			expendBills = append(expendBills, bill)
+			expendSum = expendSum.Add(bill.Amount)
+			categoryMapping[bill.Category] = categoryMapping[bill.Category].Add(bill.Amount)
+		}
+	}
+	if len(expendBills) == 0 {
+		return ""
+	}
+
+	result := &strings.Builder{}
+	result.Write([]byte(fmt.Sprintf("合计支出：%s 元\n\n", expendSum.Abs().String())))
+	if !template.MergeCategory {
+		// 展示所有账单时，按创建时间排序
+		sort.Sort(billsSortByCreateTime(expendBills))
+		count := 0
+		for _, bill := range expendBills {
+			count++
+			result.Write([]byte(fmt.Sprintf("       - %s：%s 元", bill.Category, bill.Amount.Abs().String())))
+			if count < len(expendBills) {
+				result.Write([]byte("\n"))
+			}
+		}
+	} else {
+		count := 0
+		// 按类别支出总和由大到小排序
+		for _, category := range template.getSortedCategories(categoryMapping) {
+			amount := categoryMapping[category]
+			count++
+			result.Write([]byte(fmt.Sprintf("       - %s：%s 元", category, amount.Abs().String())))
+			if count < len(categoryMapping) {
+				result.Write([]byte("\n"))
+			}
+		}
+	}
+	return result.String()
+}
+
+// incomeSection 收入记录
+func (template *BillListTemplate) incomeSection() string {
+	var incomeBills []*models.Bill
+	incomeSum := decimal.NewFromFloat(0)
+	categoryMapping := map[string]decimal.Decimal{}
+
+	// 筛选收入账单
+	for _, bill := range template.Bills {
+		if !bill.Amount.LessThanOrEqual(decimal.NewFromFloat(0)) {
+			incomeBills = append(incomeBills, bill)
+			incomeSum = incomeSum.Add(bill.Amount)
+			categoryMapping[bill.Category] = categoryMapping[bill.Category].Add(bill.Amount)
+		}
+	}
+	if len(incomeBills) == 0 {
+		return ""
+	}
+
+	result := &strings.Builder{}
+	result.Write([]byte(fmt.Sprintf("合计收入：%s 元\n\n", incomeSum.String())))
+	if !template.MergeCategory {
+		// 展示所有账单时，按创建时间排序
+		sort.Sort(billsSortByCreateTime(incomeBills))
+		count := 0
+		for _, bill := range incomeBills {
+			count++
+			result.Write([]byte(fmt.Sprintf("       - %s：%s 元", bill.Category, bill.Amount.String())))
+			if count < len(incomeBills) {
+				result.Write([]byte("\n"))
+			}
+		}
+	} else {
+		count := 0
+		categories := template.getSortedCategories(categoryMapping)
+		// 按类别收入总和由大到小排序
+		for i := len(categories) - 1; i >= 0; i-- {
+			category := categories[i]
+			amount := categoryMapping[category]
+			count++
+			result.Write([]byte(fmt.Sprintf("       - %s：%s 元", category, amount.Abs().String())))
+			if count < len(categoryMapping) {
+				result.Write([]byte("\n"))
+			}
+		}
+	}
+	return result.String()
+}
+
+func (template *BillListTemplate) getSortedCategories(categoriesMapping map[string]decimal.Decimal) []string {
+	var sortHelper [][2]interface{}
+	for cate, amount := range categoriesMapping {
+		sortHelper = append(sortHelper, [2]interface{}{cate, amount})
+	}
+	sort.Sort(sortByAmount(sortHelper))
+	var categories []string
+	for _, item := range sortHelper {
+		categories = append(categories, item[0].(string))
+	}
+	return categories
+}
+
+type sortByAmount [][2]interface{}
+
+func (items sortByAmount) Len() int {
+	return len(items)
+}
+
+func (items sortByAmount) Less(i, j int) bool {
+	return items[i][1].(decimal.Decimal).LessThan(items[j][1].(decimal.Decimal))
+}
+
+func (items sortByAmount) Swap(i, j int) {
+	items[i], items[j] = items[j], items[i]
+}
+
+type billsSortByCreateTime []*models.Bill
+
+func (bills billsSortByCreateTime) Len() int {
+	return len(bills)
+}
+
+func (bills billsSortByCreateTime) Less(i, j int) bool {
+	return bills[i].CreatedAt.Before(bills[j].CreatedAt)
+}
+
+func (bills billsSortByCreateTime) Swap(i, j int) {
+	bills[i], bills[j] = bills[j], bills[i]
 }
--- telebot/senders_test.go
+++ telebot/senders_test.go
@@ -4,7 +4,9 @@ import (
 	"github.com/orenoid/telegram-account-bot/models"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 func TestBillCreatedTemplate_Render(t *testing.T) {
@@ -13,4 +15,81 @@ func TestBillCreatedTemplate_Render(t *testing.T) {
 
 	template = &BillCreatedTemplate{&models.Bill{Category: "购物", Amount: decimal.NewFromFloat(23.33)}}
 	assert.Equal(t, "账单金额：23.33 元，类别：购物\n点击 \"撤销\" 可撤回该账单并回滚余额", template.Render())
+}
+
+func TestBillListTemplate_Render(t *testing.T) {
+	// 测试合并统计支出
+	bills := []*models.Bill{
+		{Category: "饮食", Amount: decimal.NewFromFloat(-10)},
+		{Category: "饮食", Amount: decimal.NewFromFloat(-20.22)},
+		{Category: "出行", Amount: decimal.NewFromFloat(-10)},
+	}
+
+	template := BillListTemplate{Bills: bills, MergeCategory: true}
+	assert.Equal(t, `合计支出：40.22 元
+
+       - 饮食：30.22 元
+       - 出行：10 元`, template.Render())
+
+	// 测试合并统计收入
+	bills = []*models.Bill{
+		{Category: "工资", Amount: decimal.NewFromFloat(10000)},
+		{Category: "咸鱼", Amount: decimal.NewFromFloat(100.01)},
+		{Category: "咸鱼", Amount: decimal.NewFromFloat(50.01)},
+	}
+	template = BillListTemplate{Bills: bills, MergeCategory: true}
+	assert.Equal(t, `合计收入：10150.02 元
+
+       - 工资：10000 元
+       - 咸鱼：150.02 元`, template.Render())
+
+	// 测试同时有支出和收入的字符串拼接行为
+	bills = []*models.Bill{
+		{Category: "饮食", Amount: decimal.NewFromFloat(-10)},
+		{Category: "饮食", Amount: decimal.NewFromFloat(-20.22)},
+		{Category: "出行", Amount: decimal.NewFromFloat(-10)},
+		{Category: "工资", Amount: decimal.NewFromFloat(10000)},
+		{Category: "咸鱼", Amount: decimal.NewFromFloat(100.01)},
+		{Category: "咸鱼", Amount: decimal.NewFromFloat(50.01)},
+	}
+	template = BillListTemplate{Bills: bills, MergeCategory: true}
+	assert.Equal(t,
+		`合计支出：40.22 元
+
+       - 饮食：30.22 元
+       - 出行：10 元
+
+合计收入：10150.02 元
+
+       - 工资：10000 元
+       - 咸鱼：150.02 元`, template.Render())
+
+	template = BillListTemplate{Bills: nil}
+	assert.Equal(t, "暂无收支记录", template.Render())
+
+	// 测试订单单独展示行为（按创建时间排序）
+	now := time.Now()
+	bills = []*models.Bill{
+		{Category: "饮食", Amount: decimal.NewFromFloat(-10), Model: gorm.Model{CreatedAt: now}},
+		{Category: "饮食", Amount: decimal.NewFromFloat(-20.22), Model: gorm.Model{CreatedAt: now.Add(2 * time.Second)}},
+		{Category: "出行", Amount: decimal.NewFromFloat(-10), Model: gorm.Model{CreatedAt: now.Add(-2 * time.Second)}},
+
+		{Category: "工资", Amount: decimal.NewFromFloat(10000), Model: gorm.Model{CreatedAt: now}},
+		{Category: "咸鱼", Amount: decimal.NewFromFloat(100.01), Model: gorm.Model{CreatedAt: now.Add(-2 * time.Second)}},
+	}
+	template = BillListTemplate{Bills: bills, MergeCategory: false}
+	assert.Equal(t,
+		`合计支出：40.22 元
+
+       - 出行：10 元
+       - 饮食：10 元
+       - 饮食：20.22 元
+
+合计收入：10100.01 元
+
+       - 咸鱼：100.01 元
+       - 工资：10000 元`, template.Render())
+
+	template = BillListTemplate{Bills: nil}
+	assert.Equal(t, "暂无收支记录", template.Render())
 }
